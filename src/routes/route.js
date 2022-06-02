cbd40b17071851da97de882be59e6de4abec75ef
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-02 19:12:51 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -28,5 +28,26 @@ router.get('/test-me4', function (req, res) {
     res.send('My last api!')
 });
 
+router.get('/sol1', function (req, res) {
+    const arr =[1,2,3,5,6,7] 
+    let n = arr.length;
+    let missingNumber = Math.floor((n+1)*(n+2) / 2);
+        for (let i = 0; i < n; i++)
+            missingNumber = missingNumber- arr[i];
+
+     res.send(  { data: missingNumber  }  );
+});
+
+router.get('/sol2',function (req,res){
+    const arr = [33,34, 35, 37, 38]
+    let n = arr.length+1;
+   
+    let missingNumber
+    let first = a[i]
+    let last =
+    sumOfnConsicutive= n * (first + last) / 2
+    res.send(  { data: missingNumber  }  )
+}
+)
 module.exports = router;
 // adding this comment for no reason
