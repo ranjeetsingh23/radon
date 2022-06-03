ddf1a9395e5ea2a5876213692809fa1d91f71277
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-03 23:55:46 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,7 @@
 const express = require('express');
 const { indexOf } = require('underscore');
-// const myHelper = require('../util/helper')
-// const underscore = require('underscore')
-//const lodash = require('lodash')
 const router = express.Router();
 
-// router.get('/test-me', function (req, res) {
-//     myHelper.printDate()
-//     myHelper.getCurrentMonth()
-//     myHelper.getCohortData()
-//     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-//     console.log('The first element received from underscope function is '+firstElement)
-//     res.send('My first ever api!')
-// });
-
-// router.get('/hello', function (req, res) {
-   
-//     res.send('Hello there!')
-// });
-
-// router.get('/candidates', function(req, res){
-//     console.log('Query paramters for this request are '+JSON.stringify(req.query))
-//     let gender = req.query.gender
-//     let state = req.query.state
-//     let district = req.query.district
-//     console.log('State is '+state)
-//     console.log('Gender is '+gender)
-//     console.log('District is '+district)
-  //  let candidates = ['Akash','Suman']
-//     res.send(candidates)
-// })
-
-//  router.get('/candidates/:canidatesName', function(req, res){
-//      console.log('The request objects is '+ JSON.stringify(req.params))
-//      console.log('Candidates name is '+req.params.candidateName)
-//      res.send('Done')
-// })
 
 router.get('/movies',function(req,res){
     const arrMovies =['‘Rang de basanti’, ‘The shining’, ‘Lord of the rings’, ‘Batman begins’']
@@ -43,12 +9,11 @@ router.get('/movies',function(req,res){
 }
 )
 router.get('/movies/:indexNumber',function(req,res){
-    const arrMovies =['‘Rang de basanti’, ‘The shining’, ‘Lord of the rings’, ‘Batman begins’']
-    let num= req.params.indexNumber
-    if(num >=Array.length){
-        console.log(" use a valid index in an error message.")
+    const arrMovies =['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let num =arrMovies[req.params.indexNumber]
+    if(num !==arrMovies.length){
+       res.send(  num || " use a valid index." )
     }
-    res.send( { movie: "the movie is: " + arrMovies[num] })
 }
 )
 router.get('/films',function(req,res){
@@ -89,4 +54,3 @@ router.get('/films/:filmid',function(req,res){
        }
 })
 module.exports = router;
-// adding this comment for no reason
