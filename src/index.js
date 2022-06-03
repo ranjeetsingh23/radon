d9dfe322043ffd005cc06b1552305653c53114df
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-03 13:38:23 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+//const route = require('./first_project/routes/routes.js');
 const app = express();
 
 app.use(bodyParser.json());
