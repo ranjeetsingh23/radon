d2f61a61912199cd0ef5add45ffff02c066cc75f
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-04 16:37:51 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const BookModel = require("../models/bookModel.js")
 const UserController = require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 router.post("/addBooks", UserController.addBooks)
 
