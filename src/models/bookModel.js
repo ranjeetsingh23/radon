ce17744de90b6b087b94e1890ba7f4313544e660
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-09 14:33:20 +0530

    Updated

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
+const { required } = require('nodemon/lib/config');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
         type: ObjectId,
-        ref: "Author"
+        ref: "RanjeetAuthor",
+        required: true
     },
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher:{
+        type: ObjectId,
+        ref: "RanjeetPublisher"
+    }
 
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('RanjeetBook', bookSchema)
