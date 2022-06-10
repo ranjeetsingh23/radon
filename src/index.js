cd4ba3c32d969205d5543c4c9c98da4d46ea1011
ranjeetsingh23 <ranjeetsinghdua@gmail.com>
2022-06-10 17:55:00 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { timeStamp } = require('console');
+const moment = require('moment')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    function(req,res,next){
+       const today = moment();
+        console.log(today.format()+ ", " + req.ip + ", " + req.path);
+    }
+)
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
