86017b1a756cde3ba683b7d45faf4743f9270b0f
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-06-13 17:38:36 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const ProductController= require("../controllers/productController")
+const OrderController= require("../controllers/orderController")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
 router.get("/test-me", function (req, res) {
@@ -12,12 +13,12 @@ router.get("/test-me", function (req, res) {
 
 
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createOrder", commonMW.CheckisFreeAppUser,OrderController.createOrder  )
 
 
+router.post("/createProduct", ProductController.createProduct)
 
-
-router.post("/createUser", UserController.createUser)
+router.post("/createUser", commonMW.CheckisFreeAppUser, UserController.createUser)
 // router.get("/getUsersData", UserController.getUsersData)
 
 
@@ -48,7 +49,7 @@ router.post("/createUser", UserController.createUser)
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
